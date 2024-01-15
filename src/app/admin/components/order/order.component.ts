bdf0e77cd7f627923de6988fca64630efb0cc379
Bahri.Busch <turkbahri19@hotmail.com>
2024-01-15 16:26:02 +0300

    orders listed

--- src/app/admin/components/order/order.component.ts
+++ src/app/admin/components/order/order.component.ts
@@ -1,12 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Order } from '../../../models/order';
+import { OrderService } from '../../../services/order.service';
+import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-order',
   standalone: true,
-  imports: [],
+  imports: [CommonModule,RouterLink],
   templateUrl: './order.component.html',
   styleUrl: './order.component.scss'
 })
-export class OrderComponent {
+export class OrderComponent implements OnInit {
+  orders:Order[]=[]
+  constructor(private orderService:OrderService){}
+  ngOnInit(): void {
+    this.getList();
+  }
 
+  getList(){
+    this.orderService.getAll().subscribe(res=>{
+      this.orders=res.data
+    })
+  }
 }
--- src/app/services/order.service.ts
+++ src/app/services/order.service.ts
@@ -1,11 +1,13 @@
 import { Injectable } from '@angular/core';
 import { BaseService } from './base/base.service';
-import { Order } from '../models/order';
+import { Order, OrderWithProduct } from '../models/order';
+import { Observable } from 'rxjs';
+import { DataResponse } from '../models/responses';
 
 @Injectable({
   providedIn: 'root'
 })
 export class OrderService extends BaseService<Order> {
   override path: string="orders";
- 
+  // getByIdWithProducts(productId:number):Observable<DataResponse<OrderWithProduct>>
 }
